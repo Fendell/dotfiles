b8594adb407338a326d629f4f4510e4890fe28e0
Lars Fendell <lars.fendell@gmail.com>
2019-02-10 15:09:00 +0100

    Ändrat font och installerat projectile

--- init.el
+++ init.el
@@ -11,18 +11,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (notmuch which-key yasnippet use-package try powerline org-gcal moe-theme modern-cpp-font-lock magit ggtags flycheck evil-leader company-irony))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
